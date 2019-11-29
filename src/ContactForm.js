ab59a205d5323a43c09513ea22e3f0769d8444f7
Yusent Chig <yusent@protonmail.com>
2019-11-28 21:29:46 -0800

    Add ContactForm component

---
+++ src/ContactForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import MaterialInput from './components/MaterialInput';
+
+export default function(props) {
+  const className = props.visible ? 'modal-wrapper' : 'modal-wrapper display-none';
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+
+  return (
+    <div className={className}>
+      <div className="overlay" onClick={props.onOverlayClick} />
+
+      <div className="modal-content">
+        <div className="modal-body">
+          <MaterialInput
+            label="First Name"
+            onChange={setFirstName}
+            value={firstName}
+          />
+
+          <MaterialInput
+            label="Last Name"
+            onChange={setLastName}
+            value={lastName}
+          />
+
+          <MaterialInput
+            label="Email"
+            onChange={setEmail}
+            value={email}
+          />
+        </div>
+
+        <div className="modal-actions">
+          <button>
+            accept
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
